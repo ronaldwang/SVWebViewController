48531fe3ecb578f84853cdba5368706caadcd434
ronald wang <kingfeng@gmail.com>
2014-09-14 19:54:33 +0900

    0.2.0

---
+++ Demo/Classes/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  RootViewController.m
+//  SVWebViewController
+//
+//  Created by Sam Vermette on 21.02.11.
+//  Copyright 2011 Sam Vermette. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "SVWebViewController.h"
+
+@implementation ViewController
+
+
+- (void)pushWebViewController {
+    NSURL *URL = [NSURL URLWithString:@"http://en.wikipedia.org/wiki/Friday_(Rebecca_Black_song)"];
+	SVWebViewController *webViewController = [[SVWebViewController alloc] initWithURL:URL];
+	[self.navigationController pushViewController:webViewController animated:YES];
+}
+
+
+- (void)presentWebViewController {
+	NSURL *URL = [NSURL URLWithString:@"http://en.wikipedia.org/wiki/Friday_(Rebecca_Black_song)"];
+	SVModalWebViewController *webViewController = [[SVModalWebViewController alloc] initWithURL:URL];
+	webViewController.modalPresentationStyle = UIModalPresentationPageSheet;
+    webViewController.availableActions = SVWebViewControllerAvailableActionsOpenInSafari | SVWebViewControllerAvailableActionsOpenInChrome | SVWebViewControllerAvailableActionsCopyLink | SVWebViewControllerAvailableActionsMailLink;
+	[self presentModalViewController:webViewController animated:YES];	
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+	return YES;
+}
+
+
+@end
+
